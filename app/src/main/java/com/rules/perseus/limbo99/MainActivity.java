e5a7e63a3a5381de61c12c9aab9215a754c9f5ed
hessfr <frederik.hess@gmail.com>
2015-01-26 22:09:20 +0100

    Initial Commit

---
+++ app/src/main/java/com/rules/perseus/limbo99/MainActivity.java
@@ -0,0 +1,214 @@
+package com.rules.perseus.limbo99;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private static final String TAG = "Mainactivity";
+
+    private TextView txtSpeechInput;
+    private ImageButton btnSpeak;
+    private SpeechRecognizer speechRecognizer;
+    private SharedPreferences mPrefs;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        txtSpeechInput = (TextView) findViewById(R.id.txtSpeechInput);
+        btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
+
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v.getId() == R.id.btnSpeak) {
+
+                    final String emptyString = "empty";
+                    String inputLanguage = mPrefs.getString("input_language", emptyString);
+                    if (inputLanguage.equals(emptyString)) {
+                        inputLanguage = "en";
+                    }
+
+                    Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, inputLanguage);
+                    intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                    intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, "voice.recognition.test");
+
+                    intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 5);
+                    speechRecognizer.startListening(intent);
+                    Log.i(TAG, "on click");
+                }
+            }
+        });
+
+        speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        speechRecognizer.setRecognitionListener(new listener());
+
+
+
+//        btnSpeak.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//                promptSpeechInput();
+//            }
+//        });
+
+    }
+
+
+//    /**
+//     * Showing google speech input dialog
+//     * */
+//    private void promptSpeechInput() {
+//
+//        final String emptyString = "empty";
+//        String inputLanguage = mPrefs.getString("input_language", emptyString);
+//        if (inputLanguage.equals(emptyString)) {
+//            inputLanguage = "en";
+//        }
+//
+//        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+//        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+//                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+//        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, inputLanguage);
+//        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+//                getString(R.string.speech_prompt));
+//        try {
+//            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+//        } catch (ActivityNotFoundException a) {
+//            Toast.makeText(getApplicationContext(),
+//                    getString(R.string.speech_not_supported),
+//                    Toast.LENGTH_SHORT).show();
+//        }
+//    }
+
+    class listener implements RecognitionListener
+    {
+        public void onReadyForSpeech(Bundle params)
+        {
+            Log.d(TAG, "onReadyForSpeech");
+        }
+        public void onBeginningOfSpeech()
+        {
+            Log.d(TAG, "onBeginningOfSpeech");
+        }
+        public void onRmsChanged(float rmsdB)
+        {
+            Log.v(TAG, "onRmsChanged");
+        }
+        public void onBufferReceived(byte[] buffer)
+        {
+            Log.d(TAG, "onBufferReceived");
+        }
+        public void onEndOfSpeech()
+        {
+            Log.d(TAG, "onEndofSpeech");
+        }
+        public void onError(int error)
+        {
+            Log.d(TAG,  "error " +  error);
+
+
+            if (error == SpeechRecognizer.ERROR_SPEECH_TIMEOUT) {
+                Toast.makeText(getApplicationContext(),
+                        getString(R.string.error_speech_timeout),
+                        Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(),
+                        getString(R.string.error),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+        public void onResults(Bundle results)
+        {
+            String str = new String();
+            Log.d(TAG, "onResults " + results);
+            ArrayList data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            for (int i = 0; i < data.size(); i++)
+            {
+                Log.d(TAG, "result " + data.get(i));
+                str += data.get(i);
+            }
+
+            try {
+                txtSpeechInput.setText((String) data.get(0));
+            } catch (Exception e) {
+
+            }
+
+            // txtSpeechInput.setText("results: " + String.valueOf(data.size()));
+        }
+        public void onPartialResults(Bundle partialResults)
+        {
+            Log.d(TAG, "onPartialResults");
+        }
+        public void onEvent(int eventType, Bundle params)
+        {
+            Log.d(TAG, "onEvent " + eventType);
+        }
+    }
+
+//    /**
+//     * Receiving speech input
+//     * */
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        switch (requestCode) {
+//            case REQ_CODE_SPEECH_INPUT: {
+//                if (resultCode == RESULT_OK && null != data) {
+//
+//                    ArrayList<String> result = data
+//                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+//                    txtSpeechInput.setText(result.get(0));
+//                }
+//                break;
+//            }
+//
+//        }
+//    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Take appropriate action for each action item click
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent i = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(i);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
---
+++ app/src/main/java/com/rules/perseus/limbo99/SettingsActivity.java
@@ -0,0 +1,118 @@
+package com.rules.perseus.limbo99;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.Toast;
+
+
+public class SettingsActivity extends Activity {
+
+    private RadioGroup radioLanguage;
+    private RadioButton radioLanguageButton;
+    private SharedPreferences mPrefs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        radioLanguage = (RadioGroup) findViewById(R.id.radioLanguage);
+
+        /*
+        Select the right radio button (of the currently selected language) when creating
+        the radio buttons:
+        */
+        final String emptyString = "empty";
+        String inputLanguage = mPrefs.getString("input_language", emptyString);
+        if (inputLanguage.equals(emptyString)) {
+            inputLanguage = "en";
+        }
+        if (inputLanguage.equals("en")) {
+            radioLanguage.check(R.id.radioEnglish);
+        } else if (inputLanguage.equals("de")) {
+            radioLanguage.check(R.id.radioGerman);
+        } else {
+            radioLanguage.check(R.id.radioEnglish);
+        }
+
+            radioLanguage.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // find which radio button is selected
+                if(checkedId == R.id.radioGerman) {
+                    // Write to shared preferences:
+                    SharedPreferences.Editor editor = mPrefs.edit();
+                    editor.putString("input_language", "de");
+                    editor.commit();
+                    Toast.makeText(getApplicationContext(), "german",
+                            Toast.LENGTH_SHORT).show();
+                } else if(checkedId == R.id.radioEnglish) {
+                    Toast.makeText(getApplicationContext(), "english",
+                            Toast.LENGTH_SHORT).show();
+                    SharedPreferences.Editor editor = mPrefs.edit();
+                    editor.putString("input_language", "en");
+                    editor.commit();
+                }
+            }
+
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+/*    public void addListenerOnButton() {
+
+        radioLanguage = (RadioGroup) findViewById(R.id.radioLanguage);
+
+        radioLanguageButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                // get selected radio button from radioGroup
+                int selectedId = radioLanguage.getCheckedRadioButtonId();
+
+                // find the radiobutton by returned id
+                radioLanguageButton = (RadioButton) findViewById(selectedId);
+
+                Toast.makeText(SettingsActivity.this,
+                        radioLanguageButton.getText(), Toast.LENGTH_SHORT).show();
+
+            }
+
+        });
+    }*/
+}
